3641ab9ef7d65c1cf58a1a640b37b45a31936d7c
devhipo <ghdmsrkd@naver.com>
2021-09-23 17:38:38 +0900

    add : game 풀이 추가

--- 2-basic-types-challenge/game.ts
+++ 2-basic-types-challenge/game.ts
@@ -1,6 +1,18 @@
-/**
- * Let's make a game 🕹
- */
+const position = {
+  x : 0,
+  y : 0
+}
+type Command = 'up' | 'down' | 'left' | 'right'
+const operators = {
+  'up' : () => position.y += 1,
+  'down' : () => position.y -= 1,
+  'left' : () => position.x -= 1,
+  'right' : () => position.x += 1,
+}
+const move = (command : Command) => {
+  operators[command]()
+}
+
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
