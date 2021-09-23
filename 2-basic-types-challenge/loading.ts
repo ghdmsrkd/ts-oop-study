6a3cc01b5b81d5741473f038ca49845cd77e6d4d
devhipo <ghdmsrkd@naver.com>
2021-09-23 17:44:34 +0900

    add : loading 풀이 추가

--- 2-basic-types-challenge/loading.ts
+++ 2-basic-types-challenge/loading.ts
@@ -20,6 +20,15 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  function printLoginState(loadState : ResourceLoadState) {
+    if(loadState.state === "loading")
+      console.log("👀 loading...")
+    else if(loadState.state === "success")
+    console.log("😃 loaded")
+    else if(loadState.state === "fail")
+    console.log("😱 no network")
+  }
+
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
